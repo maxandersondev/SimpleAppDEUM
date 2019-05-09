be73ef98758c17830bbf67c6890553e7702c7fc5
Max Anderson <max.anderson@thinkahead.com>
2019-05-09 13:28:56 -0500

    init commit

---
+++ Podfile
@@ -0,0 +1,2 @@
+target 'Simple'
+pod 'AppDynamicsAgent'
---
+++ Simple/SimpleBridgingheader.m
@@ -0,0 +1,10 @@
+//
+//  SimpleBridgingheader.m
+//  Simple
+//
+//  Created by Max Anderson on 5/7/19.
+//  Copyright © 2019 Clean Swift LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
